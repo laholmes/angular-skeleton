772fc9ae01428cff68f0ef915040def3a294035a
Laurence Holmes <laholmes@hotmail.co.uk>
2016-02-07 21:14:22 +0000

    build work, using release file refs in index

--- src/app.main.ts
+++ src/app.main.ts
@@ -1,21 +1,17 @@
 'use strict';
+/// <reference path="../typings/browser.d.ts" />
 
-// define([
-// 	'angular',
-// 	'angularRoute'
-// ], function(angular, angularRoute) {
-    var $html = angular.element(document.getElementsByTagName('html')[0]);
-    
-    angular.element().ready(function() {
-        angular.bootstrap(document, ['app']);
-    });
-            
-    angular.module('app', ['ui.router'])
-        .config(['$stateProvider', function($stateProvider) {
-            // Now set up the states
-            $stateProvider.state('home', {
-                url: '',
-                templateUrl: 'src/home/home.html'
-            });
-        }]);
-// });
+var $html = angular.element(document.getElementsByTagName('html')[0]);
+
+angular.element().ready(function() {
+    angular.bootstrap(document, ['app']);
+});
+        
+angular.module('app', ['ui.router'])
+    .config(['$stateProvider', function($stateProvider) {
+        // Now set up the states
+        $stateProvider.state('home', {
+            url: '',
+            templateUrl: 'src/home/home.html'
+        });
+    }]);
